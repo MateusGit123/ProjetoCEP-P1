3749604666ecb25973509128546ce767d5b1624e
MateusGit123 <mateus.veloso@fatec.sp.gov.br>
2025-04-19 20:05:29 -0300

    feat(p1): cria componente principal e faz hello

---
+++ consulta-cep/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Busca from './components/Busca';
+import LocalidadeLista from './components/LocalidadeLista';
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1>Hello, P1</h1>
+      <Busca />
+      <LocalidadeLista />
+    </div>
+  );
+}
+
+export default App;
